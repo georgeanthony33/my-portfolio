b207c6e44b2b81a5fb5e193764008baf0a3c0486
georgeanthony33 <georgeanthony33@gmail.com>
2020-03-27 00:27:03 +0000

    updated portfolio

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,35 @@
 function init() {
+
+  const mobileNav = document.querySelector('.icon')
   
   function logPosition() {
-    if (window.scrollY > 520) {
-      document.documentElement.style.setProperty('--visibility', 'hidden')
+    if (window.innerWidth > 641) {
+      if (window.scrollY > 520) {
+        document.documentElement.style.setProperty('--visibility', 'hidden')
+      } else {
+        document.documentElement.style.setProperty('--visibility', 'visible')
+      }
     } else {
-      document.documentElement.style.setProperty('--visibility', 'visible')
+      if (window.scrollY > 550) {
+        document.documentElement.style.setProperty('--visibility', 'hidden')
+      } else {
+        document.documentElement.style.setProperty('--visibility', 'visible')
+      }
     }
   }
 
+  /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+  function myFunction() {
+    var x = document.querySelector('#hiddenLinks')
+    if (x.style.display === 'block') {
+      x.style.display = 'none'
+    } else {
+      x.style.display = 'block'
+    }
+  }
+
+
+  mobileNav.addEventListener('click', myFunction)
   window.addEventListener('scroll', logPosition)
 }
 
